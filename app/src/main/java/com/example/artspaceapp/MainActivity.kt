ec22ac381dfbb599f7a4583fb2db7e9174e13588
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2023-12-21 10:30:05 +0100

    erasing all structure to build it correctly again

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,33 +3,12 @@ package com.example.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,54 +30,22 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun ArtSpaceAppLayout() {
 
-        var currentImage by remember { mutableStateOf(1) }
 
-        Column(
-            modifier = Modifier
-                .statusBarsPadding()
-                .padding(20.dp)
-                .verticalScroll(rememberScrollState())
-                .safeDrawingPadding(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-
-        }
     }
 
     @Composable
-    fun ImageCadre(
-        drawableResourceId: Int,
-        contentDescriptionResourceId: Int) {
-        Image(
-            painter = painterResource(drawableResourceId),
-            contentDescription = stringResource(contentDescriptionResourceId))
+    fun ImageCadre(){
+
     }
 
     @Composable
     fun DescriptionPane() {
-        Column {
-            Text(text = "Name")
-            Text(text = "Owner")
-        }
+
     }
 
     @Composable
-    fun ButtonsPane(currentImage: Int): Int {
-        var newValueCurrentImage: Int = currentImage
-        Row {
-            Button(onClick = {
+    fun ButtonsPane(currentImage: Int) {
 
-            }) {
-                Text(text = "Previous")
-            }
-            Button(onClick = {
-
-            }) {
-                Text(text = "Next")
-            }
-        }
-        return newValueCurrentImage
     }
 
     @Preview
